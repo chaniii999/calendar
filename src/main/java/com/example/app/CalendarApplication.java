d6b561137042a978ea7a2e8b6fa859d13c9a49e3
chaniii999 <psc0729@naver.com>
2025-08-13 20:51:04 +0900

    명칭 변경

--- src/main/java/com/example/app/CalendarApplication.java
+++ src/main/java/com/example/app/CalendarApplication.java
@@ -1,13 +1,14 @@
-package com.example.demo;
+package com.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class DemoApplication {
+public class CalendarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CalendarApplication.class, args);
 	}
 
 }
