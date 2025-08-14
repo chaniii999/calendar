983a9f04155f131e535ed00fe0793543b676c3aa
chaniii999 <psc0729@naver.com>
2025-08-14 16:41:46 +0900

    add TokenDto class for encapsulating JWT token details

---
+++ src/main/java/com/calendar/app/dto/auth/TokenDto.java
@@ -0,0 +1,17 @@
+package com.calendar.app.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private Long accessTokenExpiresIn;
+}
