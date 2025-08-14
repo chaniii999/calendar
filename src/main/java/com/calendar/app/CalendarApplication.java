9065203daa82fe38cc2f4033dbffc088a75780a9
chaniii999 <psc0729@naver.com>
2025-08-14 19:09:29 +0900

    fix typo in application.yml and enable JwtProperties configuration in CalendarApplication

--- src/main/java/com/calendar/app/CalendarApplication.java
+++ src/main/java/com/calendar/app/CalendarApplication.java
@@ -1,10 +1,13 @@
 package com.calendar.app;
 
+import com.calendar.app.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class CalendarApplication {
 
 	public static void main(String[] args) {
